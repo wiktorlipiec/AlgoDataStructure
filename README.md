bdae216eafd2a8263c3ab5f766adeb055a2dab14
Wiktor Lipiec <wiktor@devanddeliver.com>
2021-12-11 11:54:47 +0100

    solution for reverse string

---
+++ README.md
@@ -0,0 +1,3 @@
+## Algorithms and data structure
+Solution exercises from course: 
+https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/
--- ReverseString/index.js
+++ ReverseString/index.js
@@ -1,3 +1,19 @@
 function reverseString(str) {
-  
+  return str.split('').reverse().join('');
+}
+
+function reverseStringWithoutMethod(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+  };
+
+  return reversed;
+}
+
+function reverseStringByReduce(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
 }
